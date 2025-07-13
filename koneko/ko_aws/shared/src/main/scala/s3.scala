7b42e3290d43f9f576b5505975d580b251b24461
Kenichi Masuko <kenichi.masuko@sysart.jp>
2025-07-14 06:37:38 +0900

    s

--- koneko/ko_aws/shared/src/main/scala/s3.scala
+++ koneko/ko_aws/shared/src/main/scala/s3.scala
@@ -15,9 +15,9 @@ import cats.data.Chain
 import java.time.Instant
 
 /*
-  2025-05
+  2025-07
     https://github.com/gnieh/fs2-data is used since
-    https://github.com/http4s/http4s-fs2-data has no releases for more than a year
+    https://github.com/http4s/http4s-fs2-data has no releases for 16 months
 */
 package object s3:
   def endpointOf(regionCode: String): Uri =
@@ -91,16 +91,16 @@ package object s3:
 
   case class CanonicalUser(
     ID: String,
-    DisplayName: Option[String], // https://docs.aws.amazon.com/AmazonS3/latest/API/API_Owner.html says this field becomes missing in 2025
+    DisplayName: Option[String], // will be missing since 2025-10 https://docs.aws.amazon.com/AmazonS3/latest/API/API_Owner.html
   )
   object CanonicalUser:
     def parser[F[_]: Sync](local: String): Parser[F, CanonicalUser] =
       for
         _           <- startTag[F](local)
         id          <- textOnlyTag[F]("ID")
-        displayName <- textOnlyTag[F]("DisplayName")
+        displayName <- textOnlyTagOpt[F]("DisplayName")
         _           <- endTag[F](local)
-      yield CanonicalUser(id, Some(displayName))
+      yield CanonicalUser(id, displayName)
 
   object ListBuckets:
     def apply[F[_]: Async](profile: Aws.Profile)(http: KoHttpClient[F, ?])(req: Request,
@@ -126,6 +126,7 @@ package object s3:
             .through(XmlParser.doc(ListAllMyBucketsResult.parser).pipe)
             .compile.onlyOrError
 
+        // get more results when continuation-token exists
         result <- (result.ContinuationToken, dontRepeatWithContinuationToken) match
           case (Some(continuationToken), false) => apply(profile)(http)
               (req.copy(`continuation-token` = Some(continuationToken)), false)
@@ -147,3 +148,104 @@ package object s3:
           s"max-buckets = ${`max-buckets`}")
 
     type Response = ListAllMyBucketsResult
+
+  case class ListBucketResult(
+    IsTruncated: Boolean,
+    Contents: Chain[ListBucket.Contents],
+    Name: String,
+    Prefix: Option[String],
+    Delimiter: Option[String],
+    MaxKeys: Option[Int],
+    CommonPrefixes: Chain[ListBucket.CommonPrefixes],
+    EncodingType: Option[String],
+    KeyCount: Option[String],
+    ContinuationToken: Option[String],
+    NextContinuationToken: Option[String],
+    StartAfter: Option[String],
+  ):
+    
+  object ListBucketResult:
+    def parser[F[_]: Sync]: Parser[F, ListBucketResult] =
+      for
+        _                     <- startTag[F]("ListAllMyBucketsResult")
+        isTruncated           <- textOnlyTag[F]("IsTruncated").map(_.toBoolean)
+        contents              <- ListBucket.Content.parser("Contents")
+        name                  <- textOnlyTag[F]("Name")
+        prefix                <- textOnlyTagOpt[F]("Prefix")
+        delimiter             <- textOnlyTagOpt[F]("Delimiter")
+        maxKeys               <- textOnlyTagOpt[F]("MaxKeys").map(_.toInt)
+        commonPrefixes        <- ListBucket.CommonPrefixes.parser("CommonPrefixes")
+        encodingType          <- textOnlyTagOpt[F]("EncodingType")
+        keyCount              <- textOnlyTagOpt[F]("KeyCount")
+        continuationToken     <- textOnlyTagOpt[F]("ContinuationToken")
+        nextContinuationToken <- textOnlyTagOpt[F]("NextContinuationToken")
+        startAfter            <- textOnlyTagOpt[F]("StartAfter")
+        _                     <- endTag[F]("ListAllMyBucketsResult")
+      yield ListAllMyBucketsResult(isTruncated, contents, name, prefix, delimiter, maxKeys,
+          )
+
+    case class Contents()
+    object Contents:
+      def parser[F[_]: Sync]: Parser[F, Contents] = ???
+
+    case class CommonPrefixes()
+    object CommonPrefixes:
+      def parser[F[_]: Sync]: Parser[F, CommonPrefixes] = ???
+
+  object ListObjectsV2:
+    def apply[F[_]: Async](profile: Aws.Profile)(http: KoHttpClient[F, ?])(req: Request,
+        dontRepeatWithContinuationToken: Boolean = false)
+        (using Logger[F]): F[Response] =
+      import profile.{credentials, region}
+      for
+        http <- http.GET("")
+          .withParam   ("list-type"         , 2)
+          .withParamOpt("continuation-token", req.`continuation-token`)
+          .withParamOpt("delimiter"         , req.`delimiter`)
+          .withParamOpt("fetch-owner"       , req.`fetch-owner`)
+          .withParamOpt("max-keys"          , req.`max-keys`)
+          .withParamOpt("prefix"            , req.`prefix`)
+          .withParamOpt("start-after"       , req.`start-after`)
+          .evalMapRequest(AwsSigV4.unsignedPayload(_, credentials, region, "s3"))
+        result <- http.acceptString.run.expectSuccess.resource.use: resp =>
+          resp.body
+            .through(text.utf8.decode)
+            .through(xml.events())
+            //.through(xml.namespaceResolver) // ignoring the namespace (AWS doc "Response Syntax" has no namespace)
+            .through(xml.referenceResolver()) // e.g. "&amp;" to "&"
+            .through(xml.normalize) // e.g. merge adjacent texts
+            .through(XmlTextTrimmer.pipe)
+            .through(XmlEventLog.pipe)
+            .through(XmlParser.doc(ListBucketResult.parser).pipe)
+            .compile.onlyOrError
+
+        // get more results when continuation-token exists
+        result <- (result.ContinuationToken, dontRepeatWithContinuationToken) match
+          case (Some(continuationToken), false) => apply(profile)(http)
+              (req.copy(`continuation-token` = Some(continuationToken)), false)
+              .map(next => result.copy(Buckets = result.Buckets ++ next.Buckets, ContinuationToken = None))
+          case _ => result.pure[F]
+
+      yield
+        result
+
+    case class Request(
+      Bucket              : String,
+      `bucket-region`     : Option[String]  = None,
+      `continuation-token`: Option[String]  = None,
+      `delimiter`         : Option[Char]    = Some('/'),
+      `encoding-type`     : Option[String]  = None,
+      `fetch-owner`       : Option[Boolean] = None,
+      `max-keys`          : Option[Int]    = None,
+      `prefix`            : Option[String]  = None,
+      `start-after`       : Option[String]  = None,
+      //x-amz-expected-bucket-owner
+      //x-amz-optional-object-attributes
+      //x-amz-request-payer
+    ):
+      require(`continuation-token`.forall(str => 0 <= str.length && str.length <= 1024),
+          s"continuation-token = ${`continuation-token`}")
+      require(`max-keys`.forall(num => 1 <= num),
+          s"max-keys = ${`max-keys`}")
+
+    type Response = ListBucketResult
