723fa6fc91ca8fd8f83b554e5a0f9d22b71835cf
Kenichi Masuko <kenichi.masuko@sysart.jp>
2025-12-15 12:04:07 +0900

    http4s: Replace failing HTTP client tests

--- koneko/ko_http4s/shared/src/test/scala/test/KoHttpRequestTests.scala
+++ koneko/ko_http4s/shared/src/test/scala/test/KoHttpRequestTests.scala
@@ -27,7 +27,78 @@ class KoHttpRequestTests extends KoCatsEffectSuite:
 
   given Logger[F] = LoggerFactory[F].getLogger
 
+  // AWS
+  nest("checkip.amazonaws.com"):
+    def api = KoHttpClient(client).withUri(uri"https://checkip.amazonaws.com") // TODO move to KoHttpClientTests.scala
+
+    test("200"):
+      for
+        myIp <- api.acceptString.GET.run.decodeSuccess.toBody
+      yield
+        assert(myIp.trim.matches("[0-9.]+"))
+
+
+  // https://developers.google.com/speed/public-dns/docs/doh/json
+  nest("dns.google"):
+    def api = KoHttpClient(client).withUri(uri"https://dns.google") // TODO move to KoHttpClientTests.scala
+
+    case class Result(
+      Status: Int, // NOERROR - Standard DNS response code (32 bit integer).
+      TC: Boolean, // Whether the response is truncated
+      RD: Boolean, // Always true for Google Public DNS
+      RA: Boolean, // Always true for Google Public DNS
+      AD: Boolean, // Whether all response data was validated with DNSSEC
+      CD: Boolean, // Whether the client asked to disable DNSSEC
+      Question: Seq[Result.Question],
+      Answer: Seq[Result.Answer],
+    )
+    object Result:
+      case class Question(
+        name: String, // FQDN with trailing dot
+        `type`: Int,  // DNS RR type
+      )
+      case class Answer(
+        name: String, // Always matches name in the Question section
+        `type`: Int,  // DNS RR type
+        TTL: Int,     // Record's time-to-live in seconds
+        data: String, // Data for A - IP address as text
+      )
+
+    import org.http4s.circe.CirceEntityDecoder.*
+    import io.circe.generic.auto.*
+
+    test("200"):
+      for
+        result <- api.acceptJson[Result].GET("resolve?name=example.com&type=A").run.decodeSuccess.toBody
+      yield
+        () // TODO check the respBody
+
+  // https://cloudflare.com/
+  nest("cloudflare.com"):
+    def api = KoHttpClient(client).withUri(uri"https://cloudflare.com") // TODO move to KoHttpClientTests.scala
+
+    test("200"):
+      for
+        respBody <- api.acceptString.GET("cdn-cgi/trace").run.decodeSuccess.toBody
+      yield
+        () // TODO check the respBody
+
+  // https://learn.microsoft.com/ja-jp/windows-server/networking/ncsi/ncsi-frequently-asked-questions
+  nest("www.msftconnecttest.com"):
+    def api = KoHttpClient(client).withUri(uri"http://www.msftconnecttest.com") // TODO move to KoHttpClientTests.scala
+
+    test("200"):
+      for
+        respBody <- api.acceptString.GET("connecttest.txt").run.decodeSuccess.toBody
+      yield
+        assertEquals(respBody, "Microsoft Connect Test")
+
+  // https://whatismyip.akamai.com/advanced?debug
+  // https://ipv4.whatismyip.akamai.com
+  // https://ifconfig.co/
+
   // https://api.zippopotam.us/
+/*
   nest("zippopotam"):
     val api = KoHttpRequest[F].withUri(uri"http://api.zippopotam.us/US/")
     val expected = """{"country": "United States", "country abbreviation": "US", "post code": "12561", "places": [{"place name": "New Paltz", "longitude": "-74.1092", "latitude": "41.7464", "state": "New York", "state abbreviation": "NY"}]}"""
@@ -84,6 +155,7 @@ class KoHttpRequestTests extends KoCatsEffectSuite:
       yield
         assertEquals(ex.status, Status.NotFound)
         assertEquals(ex.entity, "{}")
+*/
 
   nest("encode"):
     nest("JSON"):
@@ -112,9 +184,12 @@ class KoHttpRequestTests extends KoCatsEffectSuite:
       assertEquals(req.headers.get[`Content-Type`],
           Some(`Content-Type`(MediaType.multipartType("form-data", Some(boundary.value)))))
 
-  // https://httpbin.org/
+  // https://httpbun.org/
+  // 2025-12 switched from httpbin.org since it responds with "503 Service Temporarily Unavailable" half the time
+  // 2025-12 httpbun.org can be accessed from Chrome, but keep producing TLS errors
+/*
   nest("httpbin.org"):
-    val api = KoHttpRequest[F].withUri(uri"https://httpbin.org/")
+    val api = KoHttpRequest[F].withUri(uri"https://httpbun.org/")
 
     test("405"):
       for
@@ -132,6 +207,7 @@ class KoHttpRequestTests extends KoCatsEffectSuite:
       yield
         assertEquals(ex.status, Status.ServiceUnavailable)
         assertEquals(ex.entity, "")
+*/
 
   // https://jsonplaceholder.typicode.com/
   nest("jsonplaceholder.typicode.com"):
